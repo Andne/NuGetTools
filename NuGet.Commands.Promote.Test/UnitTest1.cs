697341bdfc984d3e538fb0a7d224265b7ec84969
Andy Neebel <andy@andne.net>
2014-08-21 09:44:21 -0500

    Commit a few things so I can migrate to vsonline

--- NuGet.Commands.Promote.Test/UnitTest1.cs
+++ NuGet.Commands.Promote.Test/UnitTest1.cs
@@ -6,64 +6,95 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NuGet.Commands.Promote.Test
 {
-   [TestClass]
-   public class UnitTest1
-   {
-      private string _workingDirectory;
+    [TestClass]
+    public class UnitTest1
+    {
+        private string _workingDirectory;
 
-      [TestInitialize]
-      public void LoadWorkingDirectory()
-      {
-         string full_path = Assembly.GetExecutingAssembly().Location;
-         this._workingDirectory = Path.GetDirectoryName(full_path);
-      }
+        [TestInitialize]
+        public void LoadWorkingDirectory()
+        {
+            string full_path = Assembly.GetExecutingAssembly().Location;
+            this._workingDirectory = Path.GetDirectoryName(full_path);
+        }
 
-      private Process GetNuGetProcess(string Arguments)
-      {
-         var p = new Process();
-         p.StartInfo = new ProcessStartInfo();
+        private Process GetNuGetProcess(string Arguments)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo();
 
-         p.StartInfo.FileName = "nuget.exe";
-         p.StartInfo.Arguments = Arguments;
-         p.StartInfo.WorkingDirectory = this._workingDirectory;
-         p.StartInfo.UseShellExecute = false;
-         p.StartInfo.RedirectStandardOutput = true;
-         p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = "nuget.exe";
+            p.StartInfo.Arguments = Arguments;
+            p.StartInfo.WorkingDirectory = this._workingDirectory;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
 
-         return p;
-      }
+            return p;
+        }
 
-      [TestMethod]
-      public void TestCommandRegistered()
-      {
-         var p = GetNuGetProcess("help promote");
+        [TestMethod]
+        public void TestCommandRegistered()
+        {
+            var p = GetNuGetProcess("help promote");
 
-         p.Start();
+            p.Start();
 
-         p.WaitForExit();
-         Assert.AreEqual(0, p.ExitCode);
-      }
+            p.WaitForExit();
+            Assert.AreEqual(0, p.ExitCode);
+        }
 
-      [TestMethod]
-      public void TestHelpCommand()
-      {
-         var p = GetNuGetProcess("help promote");
+        [TestMethod]
+        public void TestHelpCommand()
+        {
+            var p = GetNuGetProcess("help promote");
 
-         p.Start();
+            p.Start();
 
-         var output = p.StandardOutput.ReadToEnd();
-         Assert.AreEqual("usage: NuGet promote <packageId>", output.Substring(0, output.IndexOf("\r")));
-      }
+            var output = p.StandardOutput.ReadToEnd();
+            Assert.AreEqual("usage: NuGet promote <packageId> [options]", output.Substring(0, output.IndexOf("\r")));
+        }
 
-      [TestMethod]
-      public void TestTestCommand()
-      {
-         var p = GetNuGetProcess("promote Package1");
+        [TestMethod]
+        public void TestTestCommand()
+        {
+            var expected_file = Path.Combine(this._workingDirectory, "DestRepo", "ExamplePackage.1.0.0-CI01.nupkg");
+            if (File.Exists(expected_file))
+            {
+                File.Delete(expected_file);
+            }
 
-         p.Start();
-         var output = p.StandardOutput.ReadToEnd();
+            var p = GetNuGetProcess(
+                String.Format(@"promote ExamplePackage -Source ""{0}\SourceRepo"" -Destination ""{0}\DestRepo""",
+                this._workingDirectory)
+                );
 
-         Assert.AreEqual("Promoting package Package1\r\n", output);
-      }
-   }
+            p.Start();
+            var output = p.StandardOutput.ReadToEnd();
+
+            Assert.AreEqual("Promoting package ExamplePackage\r\n", output);
+            Assert.IsTrue(File.Exists(expected_file));
+        }
+
+        [TestMethod]
+        public void TestStripVersion()
+        {
+            var expected_file = Path.Combine(this._workingDirectory, "DestRepo", "ExamplePackage.1.0.0.nupkg");
+            if (File.Exists(expected_file))
+            {
+                File.Delete(expected_file);
+            }
+
+            var p = GetNuGetProcess(
+                String.Format(@"promote ExamplePackage -Source ""{0}\SourceRepo"" -Destination ""{0}\DestRepo"" -StripPrereleaseVersion",
+                this._workingDirectory)
+                );
+
+            p.Start();
+            var output = p.StandardOutput.ReadToEnd();
+
+            Assert.AreEqual("Promoting package ExamplePackage\r\n", output);
+            Assert.IsTrue(File.Exists(expected_file));
+        }
+    }
 }
--- NuGet.Promote.Extensions/PromoteCommand.cs
+++ NuGet.Promote.Extensions/PromoteCommand.cs
@@ -32,6 +32,27 @@ namespace NuGet.Promote
         {
             var packageId = base.Arguments[0];
 
+            var repo = RepositoryFactory.CreateRepository(this.Source);
+            IPackage package;
+
+            /*if (this.StripPrereleaseVersion)
+            {
+                
+            }
+            else*/
+            {
+                package = repo.FindPackage(packageId);
+            }
+
+            var package_server = new PackageServer(this.Destination, "NuGet Promote Command");
+            //package_server.PushPackage("", package, Convert.ToInt32(TimeSpan.FromMinutes(5.0).TotalMilliseconds));
+            var stream = new System.IO.MemoryStream();
+            var package_bytes = package.GetStream().ReadAllBytes();
+            stream.Write(package_bytes, 0, package_bytes.Length);
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+
+            package_server.PushPackage("", stream, package_bytes.Length, Convert.ToInt32(TimeSpan.FromMinutes(5.0).TotalMilliseconds));
+
             Console.WriteLine("Promoting package {0}", packageId);
         }
     }
